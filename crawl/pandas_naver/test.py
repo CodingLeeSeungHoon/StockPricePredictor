0eb9c6428f8e765d302fc723cc9ffd08db02c1f2
sktkddn777 <sktkddn777@naver.com>
2021-12-03 20:47:10 +0900

    test naver api

--- crawl/pandas_naver/test.py
+++ crawl/pandas_naver/test.py
@@ -8,7 +8,7 @@ def naver_reader(co_date, start, end):
     df = df.read()
     print(df)
 if __name__ == "__main__":
-    df = web_naver.NaverDailyReader(symbols='005930', start='20201111', end='20211114', adjust_price=True)
+    df = web_naver.NaverDailyReader(symbols='005930', start='20210915', end='20210923', adjust_price=True)
 
     df = df.read()
     print(df)
