143c84ef9f54acf216443b47e3cba94a09a7a494
sktkddn777 <sktkddn777@naver.com>
2021-12-07 01:23:08 +0900

    12/07

--- preprocess/Stopwords.py
+++ preprocess/Stopwords.py
@@ -1,11 +1,10 @@
+from konlpy.tag import Kkma
+import re
+
 class Stopwords:
     def __init__(self):
         self.delete_stopwords = []
         self.replace_stopwords = []
-
-        """ init stopwords list """
-        self._init_delete_stopwords()
-        self._init_replace_stopwords()
 
         stopwords_file = open("stopwords.txt", 'r', encoding='UTF8')
         stopwords = []
@@ -14,18 +13,30 @@ class Stopwords:
             words = words.strip()
             stopwords.append(words)
 
-    def _init_delete_stopwords(self, title_str):
+    def _get_delete_stopwords(self, title_str):
+        """
+        title_str이 들어와서 stopwords를 제거하고
+        토큰화를 해서
+        제거한 문장 토큰 리스트 형태로 반환.
+        """
         result_tokens = []
+        kkma = Kkma()
         title_tokens = kkma.morphs(title_str)
         for i in title_tokens:
             if i not in self.stopwords:
                 result_tokens.append(i)
         return result_tokens
 
-    def _init_replace_stopwords(self, title_str):
+    def _get_replace_stopwords(self, title_token_list):
+        """
+        title_str을 읽어 들여서
+        replace 될 단어를 찾아 replace를 해주고 해당 문장을 반환해준다.
+        @return : string
+        """
         replace_dict = {'比': '대비', '↓': '감소', '↑': '증가', 'Q': '분기', '中': '중국', '日': '일본', '美': '미국', '韓': '한국',
-                        '北': '북한', '$': '달러', '￥': '엔', '€': '유로'}
-        replace_table = title_str.maketrans(replace_dict)
-        title_str = re.sub('[0-9]', '', title_str)
-        title_str = title_str.translate(replace_table)
-        return title_str
+                            '北': '북한', '$': '달러', '￥': '엔', '€': '유로'}
+        replace_table = title_token_list.maketrans(replace_dict)
+        title_token_list = re.sub('[0-9]', '', title_token_list)
+        title_token_list = title_token_list.translate(replace_table)
+        return title_token_list
+
